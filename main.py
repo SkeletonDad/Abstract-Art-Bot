b357d06d84f4178f36d5b118068b7851d524bcdf
Joseph Styers <styersjs@gmail.com>
2024-01-23 22:06:08 -0500

    Sends message on user leaving voice channel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Final
 import os 
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
+from discord.ext import commands
 from responses import get_response
 
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
@@ -11,6 +12,7 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 # STEP 1: BOT SETUP
 intents: Intents = Intents.default()
 intents.message_content = True # NOQA (no quality assurance)
+intents.voice_states = True
 client: Client = Client(intents=intents)
 
 # STEP 2: MESSAGE FUNCTIONALITY
@@ -19,12 +21,14 @@ async def send_message(message: Message, user_message: str) -> None:
         print('(Message was empty because intents were not enabled..probably)')
         return
     
+    #is_private is true if first character is '?'
     if is_private := user_message[0] == '?':
         user_message = user_message[1:]
-
+    
     try:
         response: str = get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
+    
     except Exception as e:
         print(e)
 
@@ -34,18 +38,27 @@ async def send_message(message: Message, user_message: str) -> None:
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
 
-# STEP 4: HANDLING INCOMING MESSAGES
-    @client.event
-    async def on_message(message: Message) -> None:
-        if message.author == client.user:
-            return
+# STEP 4: HANDLING INCOMING MESSAGES AND VOICE ACTIVITY
+@client.event
+async def on_message(message: Message) -> None:
+    
+    #if the bot sent a message, ignore its own message
+    if message.author == client.user:
+        return
         
-        username: str = str(message.author)
-        user_message: str = message.content
-        channel: str = str(message.channel)
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
 
-        print(f'[{channel}] {username}: "{user_message}"')
-        await send_message(message, user_message)
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel and not after.channel:  # Member left a voice channel
+        channel = client.get_channel(744744567680794657) # ID of general channel
+        await channel.send('Who *was* that?')
+
 
 # STEP 5: MAIN ENTRY POINT
 def main() -> None:
